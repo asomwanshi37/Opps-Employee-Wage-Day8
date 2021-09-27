b0fd8f0425c55ccd6610d497cabdfb6ccf70890c
asomwanshi37 <asomwanshi37@gmail.co.com>
2021-09-27 17:05:22 +0530

    Switch Cases Problem

---
+++ EmpWage Switch Cases Day8/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EmpWage_Switch_Cases_Day8
+{
+    class Program
+    {
+        static void Main (string[]args)
+        {
+            SwitchCases.EmpWageUsingSwitch();
+        }
+
+        
+    }
+}
