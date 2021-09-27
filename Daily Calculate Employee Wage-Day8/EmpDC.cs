10e4f506d3b97a273707528fda4a9d5e90bb050f
asomwanshi37 <asomwanshi37@gmail.co.com>
2021-09-27 16:22:46 +0530

    Showing Daily Calculate EmployeeWage

---
+++ Daily Calculate Employee Wage-Day8/EmpDC.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Daily_Calculate_Employee_Wage_Day8
+{
+   public class EmpDC
+    {
+        public static void CalculateDailyEmpWage()
+        {
+            //Constant
+            int IsFullTime = 1;
+            int EMPRATEPERHOUR = 20;
+            // Variables
+            int empHrs = 0;
+            int empWage = 0;
+            Random random = new Random();
+            // Computation
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IsFullTime)
+            {
+                empHrs = 8;
+            }
+            else
+            {
+                empHrs = 0;
+            }
+            empWage = empHrs * EMPRATEPERHOUR;
+            Console.WriteLine("Emp Wage :" + empWage);
+        }
+    }
+}
---
+++ Daily Calculate Employee Wage-Day8/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Daily_Calculate_Employee_Wage_Day8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmpDC.CalculateDailyEmpWage();
+            ////Constant
+            //int IsFullTime = 1;
+            //int EMPRATEPERHOUR = 20;
+            //// Variables
+            //int empHrs = 0;
+            //int empWage = 0;
+            //Random random = new Random();
+            //// Computation
+            //int empCheck = random.Next(0, 2);
+            //if (empCheck == IsFullTime)
+            //{
+            //    empHrs = 8;
+            //}
+            //else
+            //{
+            //    empHrs = 0;
+            //}
+            //empWage = empHrs * EMPRATEPERHOUR;
+            //Console.WriteLine("Emp Wage :" + empWage);
+        }
+    }
+}
