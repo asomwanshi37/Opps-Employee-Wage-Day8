e8bd19f994a9ff6c2210436436a2509be144ba51
asomwanshi37 <asomwanshi37@gmail.co.com>
2021-09-27 12:05:20 +0530

    Showing Employee is Present or Absent

---
+++ EmployeeWageUC1/EmpPre.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeWageUC1
+{
+    public class EmpPre
+    {
+        public static void CheckPresentAbsent()
+        {
+            // Constant
+            int IsFullTime = 1; 
+            Random random = new Random();
+            // Computation
+            int empCheck = random.Next(0, 2);
+
+            if (empCheck == IsFullTime)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+    
+}
---
+++ EmployeeWageUC1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace EmployeeWageUC1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmpPre.CheckPresentAbsent(); 
+           //*/ // Constant
+           // int IsFullTime = 1;
+           // Random random = new Random();
+           // // Computation
+           // int empCheck = random.Next(0, 2);
+
+           // if (empCheck == IsFullTime)
+           // {
+           //     Console.WriteLine("Employee is Present");
+           // }
+           // else
+           // {
+           //     Console.WriteLine("Employee is Absent");
+           // }
+        }
+    }
+}
