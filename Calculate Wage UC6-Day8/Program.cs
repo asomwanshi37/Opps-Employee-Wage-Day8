d7eb3f08cf02b2a0a2aaecf2529fcfdf15faea5b
asomwanshi37 <asomwanshi37@gmail.co.com>
2021-09-27 17:38:06 +0530

    Calculate-Wages-Reached-100Hrs

---
+++ Calculate Wage UC6-Day8/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Calculate_Wage_UC6_Day8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmpWageUC6.CalculateWageTill100HrsOr20IsReached();
+        }
+    }
+}
