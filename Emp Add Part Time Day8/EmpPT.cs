991da641140340d89fcf07590f0352104bff8981
asomwanshi37 <asomwanshi37@gmail.co.com>
2021-09-27 16:28:18 +0530

    Showing Part Time Employee Wage

---
+++ Emp Add Part Time Day8/EmpPT.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Emp_Add_Part_Time_Day8
+{
+    public class EmpPT
+    {
+        public static void AddPartTimeEmpW()
+        {
+            // Constant variables
+            int IsPartTime = 1;
+            int EmpRatePerHour = 20;
+
+            //Variables
+            int EmpHrs = 0;
+            int EmpWage = 0;
+            Random random = new Random();
+
+            int EmpCheck = random.Next(1, 2);
+            Console.WriteLine("Random number :" + EmpCheck);
+
+            if (EmpCheck == IsPartTime)
+            {
+                EmpHrs = 8;
+            }
+            else
+            {
+                EmpHrs = 0;
+
+            }
+            EmpWage = EmpHrs * EmpRatePerHour;
+            Console.WriteLine("Emp Wage :" + EmpWage);
+        }
+    }
+}
