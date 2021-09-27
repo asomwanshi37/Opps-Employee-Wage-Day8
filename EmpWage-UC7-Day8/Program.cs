2af20cc586f9d610a06184628cdec3e3eaa953c6
asomwanshi37 <asomwanshi37@gmail.co.com>
2021-09-27 17:41:06 +0530

    EmpWages Calculate For Using Class Method

---
+++ EmpWage-UC7-Day8/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmpWage_UC7_Day8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmpWagw_UC7.ComputeEmpWage();
+        }
+    }
+}
