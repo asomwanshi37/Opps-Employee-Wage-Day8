3178a9774d788a45b7849fb497947a36dc9eb2dd
asomwanshi37 <asomwanshi37@gmail.co.com>
2021-09-27 17:33:31 +0530

    Calculate EmpWage for 20Days in Month

---
+++ Calculate Wage For Month Day8/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Calculate_Wage_For_Month_Day8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MonthWage.CalculateWagesFor20DaysInMonth();
+        }
+    }
+}
